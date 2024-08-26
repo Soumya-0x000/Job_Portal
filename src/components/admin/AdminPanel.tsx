d627a9b8f01aa7fd3e2070fd7bd74713592b0205
Soumya Sankar Das <soumyadas429@gmail.com>
2024-08-26 14:58:27 +0530

    updated

--- src/components/admin/AdminPanel.tsx
+++ src/components/admin/AdminPanel.tsx
@@ -17,7 +17,7 @@ const AdminPanel: FC = () => {
         setUserName(admin?.username)
 
         if(uniqueId !== savedToken) {
-            navigate('/')
+            navigate('/');
         } else {
             (async() => {
                 const { data, status } = await axios.get(`${URL}/users/me`, {
